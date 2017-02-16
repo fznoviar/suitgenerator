5366368a0c6e7f867eb73f4144aa4106ad4a3d5c
fznoviar <fznoviar@gmail.com>
2017-02-16 13:29:42 +0700

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # suitgenerator
 
-Model, Controller, View Generator for SuitCMS, still alpha version
+Model, Controller, View Generator from migration for SuitCMS, still alpha version
 
 * [Installation](#installation)
 * [Use](#use)
+
 
 ## Installation
 
@@ -20,4 +21,31 @@ Then, update `config/app.php` by adding an entry for the service provider.
     // ...
     Fznoviar\SuitGenerator\SuitGeneratorServiceProvider::class,
 ];
+```
+
+
+## Use
+
+For generate model from a table,
+
+```shell
+php artisan suit:model --table=posts
+```
+
+For generate controller from 2 tables,
+
+```shell
+php artisan suit:controller --table=posts,gallery_categories
+```
+
+For generate view from all tables,
+
+```shell
+php artisan suit:view --all
+```
+
+For generate whole mvc from a table,
+
+```shell
+php artisan suit:module --table=posts
 ```
--- src/Commands/GenerateModuleCommand.php
+++ src/Commands/GenerateModuleCommand.php
@@ -17,11 +17,7 @@ class GenerateModuleCommand extends BaseCommand
     protected $signature = 'suit:module 
                             {--table= : a single table or a list of tables separated by a comma (,)}
                             {--all : run for all tables}
-                            {--connection= : database connection to use, default use from .env connection}
-                            {--model : run generate model}
-                            {--controller : run generate controller}
-                            {--view : run generate view}
-                            {--complete : run generate model, controller, view}';
+                            {--connection= : database connection to use, default use from .env connection}';
 
     /**
      * The console command description.
@@ -54,31 +50,12 @@ class GenerateModuleCommand extends BaseCommand
             return;
         }
 
-        $tables = $this->getTables();
-
-        foreach ($tables as $table) {
-            if ($this->options['complete']) {
-                $this->info('Generate Model');
-                Artisan::call('suit:model', $this->getParams());
-                $this->info('Generate Controller');
-                Artisan::call('suit:controller', $this->getParams());
-                $this->info('Generate View');
-                Artisan::call('suit:view', $this->getParams());
-                continue;
-            }
-            if ($this->options['model']) {
-                $this->info('Generate Model');
-                Artisan::call('suit:model', $this->getParams());
-            }
-            if ($this->options['controller']) {
-                $this->info('Generate Controller');
-                Artisan::call('suit:controller', $this->getParams());
-            }
-            if ($this->options['view']) {
-                $this->info('Generate View');
-                Artisan::call('suit:view', $this->getParams());
-            }
-        }
+        $this->info('Generate Model');
+        Artisan::call('suit:model', $this->getParams());
+        $this->info('Generate Controller');
+        Artisan::call('suit:controller', $this->getParams());
+        $this->info('Generate View');
+        Artisan::call('suit:view', $this->getParams());
 
         $this->info('Complete Controller Generate Command');
     }
@@ -96,18 +73,5 @@ class GenerateModuleCommand extends BaseCommand
             $params['--connection'] = '--connection';
         }
         return $params;
-    }
-
-    protected function getOptions()
-    {
-        parent::getOptions();
-
-        $this->options['model'] = $this->option('model') ? true : false;
-        
-        $this->options['controller'] = $this->option('controller') ? true : false;
-        
-        $this->options['view'] = $this->option('view') ? true : false;
-        
-        $this->options['complete'] = $this->option('complete') ? true : false;
     }
 }
